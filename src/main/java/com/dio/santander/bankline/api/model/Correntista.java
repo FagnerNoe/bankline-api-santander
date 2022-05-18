d861339db52978acb3b7186ee03c0c6422c2877a
Fagner Noe <96452626+FagnerNoe@users.noreply.github.com>
2022-05-17 23:52:29 -0300

    repositorios

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,11 +1,29 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="table_correntista")
 public class Correntista {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)/*o banco gerencia cada registro de chave*/
 	private Integer id;
+	
+	@Column(length = 20)  /*tabelas*/
 	private String cpf;
+	
+	@Column(length = 60)
 	private String nome;
 	
+	@Embedded     
 	private Conta conta;
+	
 	public Conta getConta() {
 		return conta;
 	}
